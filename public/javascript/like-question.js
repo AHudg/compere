acab4458391191ff6f81347bc84059498b2bd8a9
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-09-04 21:27:10 -0500

    Added profile code

--- public/javascript/like-question.js
+++ public/javascript/like-question.js
@@ -1,10 +1,12 @@
 async function likeQuiz(event) {
   const emblem = event.target;
-  console.log(emblem);
   if (emblem.tagName.toLowerCase() === "i") {
     if (emblem.classList.contains("fa-regular")) {
       emblem.classList.remove("fa-regular");
       emblem.classList.add("fa-solid");
+      // emblem.value = "liked";
+      const quizId = document.getElementById("1");
+      const quizTitle = document.getElementById("quiz-title");
     } else {
       emblem.classList.remove("fa-solid");
       emblem.classList.add("fa-regular");
